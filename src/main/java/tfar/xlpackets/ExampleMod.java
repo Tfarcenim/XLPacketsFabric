96269cdaeddea8916690cf7a67a1f70e1a435186
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2020-06-16 14:26:15 -0400

    done?

---
+++ src/main/java/tfar/xlpackets/ExampleMod.java
@@ -0,0 +1,9 @@
+package tfar.xlpackets;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleMod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/tfar/xlpackets/mixin/PacketInflaterMixin.java
@@ -0,0 +1,14 @@
+package tfar.xlpackets.mixin;
+
+import net.minecraft.network.PacketInflater;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(PacketInflater.class)
+public class PacketInflaterMixin {
+	@ModifyConstant(method = "decode",constant = @Constant(intValue = 2097152))
+	private int xlPackets(int old) {
+		return Integer.MAX_VALUE;
+	}
+}
