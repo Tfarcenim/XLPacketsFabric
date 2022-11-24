a1fc9ae05806a7bdecd8208acd1adb01680a00a0
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2022-11-24 13:50:42 -0500

    1.18.2 port

--- src/main/java/tfar/xlpackets/mixin/PackeEncoderMixin.java
+++ src/main/java/tfar/xlpackets/mixin/PackeEncoderMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(PacketEncoder.class)
 public class PackeEncoderMixin {
-	@ModifyConstant(method = "encode",constant = @Constant(intValue = 2097152))
+	@ModifyConstant(method = "encode(Lio/netty/channel/ChannelHandlerContext;Lnet/minecraft/network/protocol/Packet;Lio/netty/buffer/ByteBuf;)V",constant = @Constant(intValue = 8388608))
 	private int xlPackets(int old) {
 		return 2000000000;
 	}
